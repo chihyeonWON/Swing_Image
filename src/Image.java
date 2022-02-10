26c808de0b740d8d057cfc6430e434cec8af7f89
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-10 17:06:43 +0900

    JPanel를 상속받는 ImagePanel 클래스 생성

--- src/Image.java
+++ src/Image.java
@@ -1,4 +1,22 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+class ImagePanel extends JPanel{
+	 private Image img;
+	 
+	 public ImagePanel(Image img) {
+		 this.img = img;
+		 setSize(new Dimension(img.getWidth(null),img.getHeight(null)));
+		 setLayout(null);
+	 }
+	 
+	 public void paintComponent(Graphics g) {
+		 g.drawImage(img, 0, 0, null);
+	 }
+}
 
 public class Image {
-
+	
 }
