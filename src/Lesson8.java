c7d7f249130ee5d32877e9e600c8bdf033cd2fcc
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-13 15:46:05 +0900

    이미지 추가

--- src/Lesson8.java
+++ src/Lesson8.java
@@ -1,6 +1,8 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -10,6 +12,7 @@ class ImagePanel extends JPanel{
 	 public ImagePanel(Image img) {
 		 this.img = img;
 		 setSize(new Dimension(img.getWidth(null),img.getHeight(null)));
+		 setPreferredSize(new Dimension(img.getWidth(null),img.getHeight(null)));
 		 setLayout(null);
 	 }
 	 
@@ -18,12 +21,18 @@ class ImagePanel extends JPanel{
 	 }
 }
 
-public class Image {
+public class Lesson8 {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Input img");
 		frame.setSize(640, 480);
 		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		
+		ImagePanel panel = new ImagePanel(new ImageIcon("./image/sky.jpg").getImage());
+		frame.add(panel);
+		frame.pack();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 }
